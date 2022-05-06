6816f9e3497e2b47ff10d3e050831ee6e15552bf
Zobachov Vladimir <zobachov@gmail.com>
2022-05-06 21:28:23 +0500

    fix: valid bam

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,12 @@
 const showInputError = (formElement, inputElement, errorMessage, {errorClass, inputErrorClass,...rest}) => {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    const errorElement = formElement.querySelector(`.${inputElement.id}`);
     inputElement.classList.add(inputErrorClass);
     errorElement.textContent = errorMessage;
     errorElement.classList.add(errorClass);
 };
 
 const hideInputError = (formElement, inputElement, {errorClass, inputErrorClass,...rest}) => {
-    const errorElement = formElement.querySelector(`.${inputElement.id}_error`);
+    const errorElement = formElement.querySelector(`.${inputElement.id}`);
     inputElement.classList.remove(inputErrorClass);
     errorElement.classList.remove(errorClass);
     errorElement.textContent = '';
