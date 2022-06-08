da7cdf83ce26d4b8b8c008348ca9846456532ac8
Zobachov Vladimir <zobachov@gmail.com>
2022-06-08 22:02:30 +0500

    fix: change webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// подключите к проекту mini-css-extract-plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: ''
+    },
+    devtool: "eval-source-map",
+    mode: 'development',
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+                test: /\.css$/,
+                // при обработке этих файлов нужно использовать
+                // MiniCssExtractPlugin.loader и css-loader
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader'
+                }]
+            }
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+    ]
+};
